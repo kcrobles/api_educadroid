8186536e7aea12b9147f72dcff6270df58f4301c
Kenny Chancafe <kendu.script@gmail.com>
2017-06-03 17:13:00 -0300

    modelo encuesta

--- app/Models/Domicilio.php
+++ app/Models/Domicilio.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- *  Roles
+ *  Domicilios
  */
 class Domicilio extends Model
 {
---
+++ app/Models/Encuesta.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ *  Encuestas
+ */
+class Encuesta extends Model
+{
+	protected $table = 'encuestas';
+    public function preguntas()
+    {
+    	return $this->hasMany('App\Models\Pregunta');
+    }
+}

4e20267ae136cd73091706aafcd6e56dbbd9d2ee
Kenny Chancafe <kendu.script@gmail.com>
2017-06-03 17:03:05 -0300

    controlador de domicilios

---
+++ app/Controllers/DomicilioController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controllers;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Controllers\Controller;
+use App\Models\Domicilio;
+
+class RolController extends Controller {
+
+	public function create(Request $request, Response $response)
+	{
+		$domicilio = new Domicilio;
+		$domicilio->direccion = ucwords(strtolower($request->getParam('direccion')));
+		$domicilio->latitud = $request->getParam('latitud');
+		$domicilio->longitud = $request->getParam('longitud');
+		$domicilio->save();
+		return $response->withJson($domicilio, 201);
+	}
+
+	public function all(Request $request, Response $response)
+	{
+		$domicilios = Domicilio::all();
+		return $response->withJson($domicilios, 201);
+	}
+
+	public function update(Request $request, Response $response)
+	{
+		$domicilio = null;
+		try {			
+			$domicilio = Domicilio::findOrFail($request->getAttribute('id'));
+		} catch (ModelNotFoundException $e) {
+			return $response->withJson(["message" => "Registro de domicilio no encontrado"], 404);
+		}
+		$domicilio->direccion = ($request->getParam('direccion') != null) ? ucwords(strtolower($request->getParam('direccion'))) : $domicilio->direccion;
+		$domicilio->latitud = ($request->getParam('latitud') != null) ? $request->getParam('latitud') : $domicilio->latitud;
+		$domicilio->longitud = ($request->getParam('longitud') != null ? $request->getParam('longitud') : $domicilio->longitud;
+		$domicilio->save();
+		return $response->withJson(["message" => "Se actualizó el registro"], 200);
+	}
+
+	public function delete(Request $request, Response $response)
+	{
+		$domicilio = null;
+		try {
+			$domicilio = Domicilio::findOrFail($request->getAttribute('id'));
+		} catch (ModelNotFoundException $e) {
+			return $response->withJson(["message" => "Registro de domicilio no encontrado"], 404);
+		}
+		$domicilio->delete();
+		return $response->withJson(["message" => "Se eliminó el registro"], 200);
+	}
+}

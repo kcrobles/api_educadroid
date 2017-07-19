023cbbd4fc54535418355fafe2f86edf1097283f
Kenny Chancafe <kendu.script@gmail.com>
2017-07-19 01:43:52 -0300

    documentando

--- src/routes.php
+++ src/routes.php
@@ -5,41 +5,201 @@ use \Psr\Http\Message\ResponseInterface as Response;
 use \App\Controllers;
 use \DavidePastore\Slim\Validation\Validation as Validacion;
 
+/**
+ * @api {get} / Ruta raiz de la API
+ * @apiName Home
+ * @apiGroup Home
+ * @apiSuccess {string} message Devuelve el nombre de la API en caso de haberse instalado y configurado con éxito.
+ */
+
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Quién te conoce papá?');
+    $response->getBody()->write('API Educadroid');
     return $response;
 });
 
-$app->get('/images/{image}', '\App\Controllers\ImageController:getImageUploaded');
-
 /* Users */
 
+/**
+ * @api {get} /users Listado de usuarios
+ * @apiName all
+ * @apiGroup Users
+ * @apiSuccess {Object[]} users Devuelve un json con los usuarios registrados en la base de datos
+ * @apiError message 404 Informa que no hay usuarios registrados.
+ * @apiVersion 0.0.1
+ */
 $app->get('/users', '\App\Controllers\UserController:all');
-$app->get('/users/{id:[1-9]+[0-9]*}', '\App\Controllers\UserController:show');
+
+/**
+ * @api {post} /users Alta de usuarios
+ * @apiName create
+ * @apiGroup Users
+ * @apiParam {string} nombre  Nombre del usuario.
+ * @apiParam {string} apellido Apellido del usuario
+ * @apiParam {string} email Email del usuario
+ * @apiParam {string {6..12} } password Password del usuario
+ * @apiParam {string {7..8} } documento Nro de documento del usuario
+ * @apiParam {Date} fnacimiento Fecha de nacimiento del usuario
+ * @apiParam {string} sexo="m" Sexo
+ * @apiParam {string} domicilio Dirección del usuario
+ * @apiParam {string} [latitud] Coordenada latitudinal de la ubicación del usuario
+ * @apiParam {string} [longitud] Coordenada longitudinal de la ubicación del usuario
+ * @apiSuccess {Object} user Devuelve un json con los datos del alta
+ * @apiError message Informa el tipo de error (validación, repetido, parámetros, etc) en el mensaje.
+ * @apiVersion 0.0.1
+ */
 $app->post('/users', '\App\Controllers\UserController:create');
+
+/**
+ * @api {get} /users/:id Mostrar usuario.
+ * @apiName show
+ * @apiGroup Users
+ * @apiParam {Number} id  ID del usuario.
+ * @apiSuccess {Object} user Devuelve un json con los datos del usuario encontrado.
+ * @apiError message 404 El usuario no existe.
+ * @apiVersion 0.0.1
+ */
+$app->get('/users/{id:[1-9]+[0-9]*}', '\App\Controllers\UserController:show');
+
+/**
+ * @api {put} /users/:id Modificar usuario
+ * @apiName update
+ * @apiGroup Users
+ * @apiParam {Number} id  ID del usuario.
+ * @apiParam {string} [nombre]  Nombre del usuario.
+ * @apiParam {string} [apellido] Apellido del usuario
+ * @apiParam {string} [email] Email del usuario
+ * @apiParam {string {6..12} } [password] Password del usuario
+ * @apiParam {string {7..8} } [documento] Nro de documento del usuario
+ * @apiParam {Date} [fnacimiento] Fecha de nacimiento del usuario
+ * @apiParam {string} [sexo] Sexo
+ * @apiParam {string} [domicilio] Dirección del usuario
+ * @apiParam {string} [latitud] Coordenada latitudinal de la ubicación del usuario
+ * @apiParam {string} [longitud] Coordenada longitudinal de la ubicación del usuario
+ * @apiSuccess {Object} user Devuelve un json con los datos actualizados del usuario
+ * @apiError message Informa el tipo de error (validación, repetido, parámetros, etc) en el mensaje.
+ * @apiVersion 0.0.1
+ */
 $app->put('/users/{id:[1-9]+[0-9]*}', '\App\Controllers\UserController:update');
+
+/**
+ * @api {delte} /users/:id Baja de usuario
+ * @apiName delete
+ * @apiGroup Users
+ * @apiParam {Number} id  ID del usuario.
+ * @apiSuccess {Object} user Devuelve un json con los datos del usuario eliminado.
+ * @apiError message 404 El usuario no existe.
+ * @apiVersion 0.0.1
+ */
 $app->delete('/users/{id:[1-9]+[0-9]*}', '\App\Controllers\UserController:delete');
 $app->get('/users/rol/{id:[1-9]+}', '\App\Controllers\UserController:getUsersByRol');
 
 /* Authentication */
 
+/**
+ * @api {post} /auth/attempt Login
+ * @apiName attempt
+ * @apiGroup Auth
+ * @apiParam {string} email Email del usuario
+ * @apiParam {string {6 .. 12} } password Password del usuario
+ * @apiSuccess {Object} token Devuelve un JWT del usuario autenticado
+ * @apiError message Informa el tipo de error (validación, repetido, contraseña, parámetros, etc) en el mensaje.
+ * @apiVersion 0.0.1
+ */
 $app->post('/auth/attempt', '\App\Controllers\Auth\AuthController:attempt')
 	->add(new Validacion($LOGIN_VALIDATORS, $translator));
 
-$app->post('/login', '\App\Controllers\Auth\AuthController:login');
-
-$app->post('/verify', '\App\Controllers\Auth\AuthController:verify');
-
 /* Roles */
-$app->post('/roles', '\App\Controllers\RolController:create');
+/**
+ * @api {get} /roles Listado de roles
+ * @apiName all
+ * @apiGroup Roles
+ * @apiSuccess {Object[]} roles Devuelve un json con los roles y sus usuarios.
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
 $app->get('/roles', '\App\Controllers\RolController:all');
+
+/**
+ * @api {post} /roles Alta de roles
+ * @apiName create
+ * @apiGroup Roles
+ * @apiParam {string} nombre Nombre del nuevo rol
+ * @apiSuccess {Object} rol Devuelve un json con los datos del registro creado
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
+$app->post('/roles', '\App\Controllers\RolController:create');
+
+/**
+ * @api {put} /roles/:id Modificar roles
+ * @apiName update
+ * @apiGroup Roles
+ * @apiParam {Number} id ID del rol
+ * @apiParam {string} nombre Nombre del rol
+ * @apiSuccess {Object} rol Devuelve un json con los datos del registro actualizado
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
 $app->put('/roles/{id:[1-9]+[0-9]*}', '\App\Controllers\RolController:update');
+
+/**
+ * @api {delete} /roles/:id Baja de roles
+ * @apiName delete
+ * @apiGroup Roles
+ * @apiParam {Number} id ID del rol
+ * @apiSuccess {Object} rol Devuelve un json con los datos del registro eliminado
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
 $app->delete('/roles/{id:[1-9]+[0-9]*}', '\App\Controllers\RolController:delete');
 
 /* Domicilios */
+/**
+ * @api {post} /domicilios Alta de domicilios
+ * @apiName create
+ * @apiGroup Domicilios
+ * @apiParam {string} direccion Direccion del usuario
+ * @apiParam {string} [latitud] Coordenada latitudinal de la ubicación del usuario
+ * @apiParam {string} [longitud] Coordenada longitudinal de la ubicación del usuario
+ * @apiSuccess {Object} domicilio Devuelve un json con los datos del registro creado
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
 $app->post('/domicilios', '\App\Controllers\DomicilioController:create');
+
+/**
+ * @api {get} /domicilios Listado de domicilios
+ * @apiName all
+ * @apiGroup Domicilios
+ * @apiSuccess {Object[]} domicilios Devuelve un json con los registros de domicilios
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
 $app->get('/domicilios', '\App\Controllers\DomicilioController:all');
+
+/**
+ * @api {put} /domicilios/:id Modificar domicilio
+ * @apiName update
+ * @apiGroup Domicilios
+ * @apiParam {number} id ID del registro de domicilio
+ * @apiParam {string} [direccion] Direccion del usuario
+ * @apiParam {string} [latitud] Coordenada latitudinal de la ubicación del usuario
+ * @apiParam {string} [longitud] Coordenada longitudinal de la ubicación del usuario
+ * @apiSuccess {Object} domicilio Devuelve un json con los datos del registro creado
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
 $app->put('/domicilios/{id:[1-9]+[0-9]*}', '\App\Controllers\DomicilioController:update');
+
+/**
+ * @api {delete} /domicilios/:id Baja de domicilio
+ * @apiName delete
+ * @apiGroup Domicilios
+ * @apiParam {number} id ID del registro de domicilio
+ * @apiSuccess {Object} domicilio Devuelve un json con los datos del registro eliminado
+ * @apiError message 404 Informa del error específico.
+ * @apiVersion 0.0.1
+ */
 $app->delete('/domicilios/{id:[1-9]+[0-9]*}', '\App\Controllers\DomicilioController:delete');
 
 /* Encuestas */
@@ -86,3 +246,13 @@ $app->get('/cursos/{id:[1-9]+[0-9]*}', '\App\Controllers\CursoController:getByUs
 
 /* Inscripciones */
 $app->post('/inscripciones', '\App\Controllers\InscripcionController:create');
+
+/**
+ * @api {get} /images/:nombre Retornar imagen
+ * @apiName getImageUploaded
+ * @apiParam {Number} nombre identificador único de la imagen subida al servidor.
+ * @apiGroup Images
+ * @apiSuccess {Object} imagen Retorna un archivo de imagen subida al servidor.
+ * @apiVersion 0.0.1
+ */
+$app->get('/images/{image}', '\App\Controllers\ImageController:getImageUploaded');
